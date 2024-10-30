cd723fb47a8b51ae9b4ec0336cc92f89bd5e0db5
Ewen Le Bihan <hey@ewen.works>
2024-10-30 12:26:22 +0100

    feat(messages): add missing event types

--- events.go
+++ events.go
@@ -5,11 +5,19 @@ import "time"
 type Event string
 
 const (
-	EventNewTicket       Event = "new_ticket"
-	EventNewPost         Event = "new_post"
-	EventGodchildRequest Event = "godchild_request"
-	EventNewComment      Event = "new_comment"
-	EventCommentReply    Event = "comment_reply"
+	EventNewTicket        Event = "new_ticket"
+	EventNewPost          Event = "new_post"
+	EventGodchildRequest  Event = "godchild_request"
+	EventNewComment       Event = "new_comment"
+	EventCommentReply     Event = "comment_reply"
+	EventCustom           Event = "custom"
+	EventTest             Event = "test"
+	EventGodchildAccepted Event = "godchild_accepted"
+	EventGodchildRejected Event = "godchild_rejected"
+	EventPendingSignup    Event = "pending_signup"
+	EventLoginStuck       Event = "login_stuck"
+	EventBookingPaid      Event = "booking_paid"
+	EventContributionPaid Event = "contribution_paid"
 )
 
 type Message struct {
@@ -18,7 +26,7 @@ type Message struct {
 	// When to push the notification
 	SendAt time.Time `json:"send_at"`
 	// Type of event that triggered the notification
-	Event Event `json:"event" jsonschema:"enum=new_ticket,enum=new_post,enum=godchild_request,enum=new_comment,enum=comment_reply"`
+	Event Event `json:"event" jsonschema:"enum=new_ticket,enum=new_post,enum=godchild_request,enum=new_comment,enum=comment_reply,enum=custom,enum=test,enum=godchild_accepted,enum=godchild_rejected,enum=pending_signup,enum=login_stuck,enum=booking_paid,enum=contribution_paid"`
 	// Churros ID of the ressource (the ticket, the post, the comment, etc)
 	// Used to determine to whom the notification should be sent
 	// For godchild_request, this is not a user id, but a godparent request id.
