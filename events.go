21f592b925bd6b06f30fb7fa803daafa01da88b2
Ewen Le Bihan <hey@ewen.works>
2024-10-30 12:45:51 +0100

    feat(messages): add missing shotgun event types

--- events.go
+++ events.go
@@ -5,19 +5,21 @@ import "time"
 type Event string
 
 const (
-	EventNewTicket        Event = "new_ticket"
-	EventNewPost          Event = "new_post"
-	EventGodchildRequest  Event = "godchild_request"
-	EventNewComment       Event = "new_comment"
-	EventCommentReply     Event = "comment_reply"
-	EventCustom           Event = "custom"
-	EventTest             Event = "test"
-	EventGodchildAccepted Event = "godchild_accepted"
-	EventGodchildRejected Event = "godchild_rejected"
-	EventPendingSignup    Event = "pending_signup"
-	EventLoginStuck       Event = "login_stuck"
-	EventBookingPaid      Event = "booking_paid"
-	EventContributionPaid Event = "contribution_paid"
+	EventNewTicket         Event = "new_ticket"
+	EventNewPost           Event = "new_post"
+	EventGodchildRequest   Event = "godchild_request"
+	EventNewComment        Event = "new_comment"
+	EventCommentReply      Event = "comment_reply"
+	EventCustom            Event = "custom"
+	EventTest              Event = "test"
+	EventGodchildAccepted  Event = "godchild_accepted"
+	EventGodchildRejected  Event = "godchild_rejected"
+	EventPendingSignup     Event = "pending_signup"
+	EventLoginStuck        Event = "login_stuck"
+	EventBookingPaid       Event = "booking_paid"
+	EventContributionPaid  Event = "contribution_paid"
+	EventShotgunOpensSoon  Event = "shotgun_opens_soon"
+	EventShotgunClosesSoon Event = "shotgun_closes_soon"
 )
 
 type Message struct {
@@ -26,7 +28,7 @@ type Message struct {
 	// When to push the notification
 	SendAt time.Time `json:"send_at"`
 	// Type of event that triggered the notification
-	Event Event `json:"event" jsonschema:"enum=new_ticket,enum=new_post,enum=godchild_request,enum=new_comment,enum=comment_reply,enum=custom,enum=test,enum=godchild_accepted,enum=godchild_rejected,enum=pending_signup,enum=login_stuck,enum=booking_paid,enum=contribution_paid"`
+	Event Event `json:"event" jsonschema:"enum=new_ticket,enum=new_post,enum=godchild_request,enum=new_comment,enum=comment_reply,enum=custom,enum=test,enum=godchild_accepted,enum=godchild_rejected,enum=pending_signup,enum=login_stuck,enum=booking_paid,enum=contribution_paid,enum=shotgun_opens_soon,enum=shotgun_closes_soon"`
 	// Churros ID of the ressource (the ticket, the post, the comment, etc)
 	// Used to determine to whom the notification should be sent
 	// For godchild_request, this is not a user id, but a godparent request id.
